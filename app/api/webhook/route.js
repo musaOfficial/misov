463f01cb44c92bee639b71566282816d04c5ce55
Musa Arslan <officialmusa58@gmail.com>
2024-04-07 14:09:08 +0200

    Message

--- app/api/webhook/route.js
+++ app/api/webhook/route.js
@@ -49,7 +49,6 @@ export async function POST(req) {
   }
  
   // Get the ID and type
-  const { id } = evt.data;
   const eventType = evt?.type;
 
   if(eventType === "user.created" || eventType === "user.updated" ){
